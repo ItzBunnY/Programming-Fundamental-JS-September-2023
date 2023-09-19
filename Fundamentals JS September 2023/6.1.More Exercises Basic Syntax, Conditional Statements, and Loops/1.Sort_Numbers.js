b18ce44690c28c4414683b36c00adc5c9aca2eda
Valentin Manafov <itzbunnyfn@gmail.com>
2023-09-19 21:40:11 +0300

    Update

--- Fundamentals JS September 2023/6.1.More Exercises Basic Syntax, Conditional Statements, and Loops/1.Sort_Numbers.js
+++ Fundamentals JS September 2023/6.1.More Exercises Basic Syntax, Conditional Statements, and Loops/1.Sort_Numbers.js
@@ -1,43 +1,50 @@
 function sort(num1, num2, num3){
 
+    let high = 0;
+    let mid = 0;
+    let low = 0;
+
     if(num1 > num2  && num1 > num3){
         if(num2 > num3){
-            console.log(num1);
-            console.log(num2);
-            console.log(num3);
+            high = num1;
+            mid = num2;
+            low = num3;
         }
         else{
-            console.log(num1);
-            console.log(num3);
-            console.log(num2)
+            high = num1;
+            mid = num3;
+            low = num2;
         }
     }
     else if(num2 > num1 && num2 > num3){
         if(num1 > num3){
-            console.log(num2);
-            console.log(num1);
-            console.log(num3);
+            high = num2;
+            mid = num1;
+            low = num3;
         }
         else{
-            console.log(num2);
-            console.log(num1);
-            console.log(num3);
+            high = num2;
+            mid = num3;
+            low = num1;
         }
         
     }
     else if(num3 > num1 && num3 > num2){
         if(num1 > num2){
-            console.log(num3);
-            console.log(num1);
-            console.log(num2);
+            high = num3;
+            mid = num1;
+            low = num2;
         }
         else{
-            console.log(num3);
-            console.log(num2);
-            console.log(num1);
+            high = num3;
+            mid = num2;
+            low = num1;
         }
         
     }
+    console.log(high);
+    console.log(mid);
+    console.log(low);
     
 }
 sort(2,1,3)
